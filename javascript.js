932050af925842c08b516adcedbe476f6d99ed52
pamela mwendwa gichunge <pgichunge@safaricom.co.ke>
2021-12-10 02:09:24 +0300

    add changes done

--- javascript.js
+++ javascript.js
@@ -3,7 +3,20 @@
 //calculating akan name 
 // formular given 
 
+function farLeft(CC) {
+    return ((CC / 4) - 2 * CC - 1);
+}
 
+function middle(YY) {
+    return (5 * YY / 4);
+}
+
+function farRight(MM) {
+    return (26 * (MM + 1) / 10);
+}
+
+var birthdayYear = Number(prompt("Which year were you born?"))
+var gender = prompt("What is your gender?")
 
 const farLeftResult = farLeft(10)
 const farRightResult = farRight(10)
@@ -13,10 +26,20 @@ var calculationResult = (farLeftResult + middleResult + farRightResult + 10) % 7
 const wholeNumber = calculationResult.toPrecision(1);
 alert(wholeNumber);
 
-//calculation results is 2
+// calculationResult = 2
 const maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"]
 const femaleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"]
 
+let result;
+if (gender == "male") {
+    result = maleNames[wholeNumber]
+} else if (gender == "female") {
+    result = femaleNames[wholeNumber]
+} else {
+    alert('Enter a valid gender')
+}
+
+alert(`Your day name is ${result}`, )
 
 const handleclick = () => {
     // gather user input 
@@ -43,8 +66,8 @@ const handleclick = () => {
         const centuryvar = string(birthyear).substring[0, 2]
         const yearvar = string(birthyear).substring[2]
         console.log(century, year)
-var birthdayYear = Number(prompt("Which year were you born?"))
-var gender = prompt("What is your gender?")
+        git
+
     
     }
     else {
