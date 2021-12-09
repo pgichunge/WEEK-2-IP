23459af71b1910ccc02ba3cba0417b4943524564
pamela mwendwa gichunge <pgichunge@safaricom.co.ke>
2021-12-10 00:35:56 +0300

    add changes

--- javascript.js
+++ javascript.js
@@ -1,18 +1,28 @@
 const handleclick=() =>{
-    // gather user input for
+    // gather user input 
 
-
-const birthmonthbox=document.getelementbyid("birthmonth")
-const birthyearbox=document.getelementbyid("birthyear")
-const birthgenderbox=document.getelementbyid("gender")
+constbirthdaybox=document.getelementbyid("birthday")
+constbirthmonthbox=document.getelementbyid("birthmonth")
+constbirthyearbox=document.getelementbyid("birthyear")
+constbirthgenderbox=document.getelementbyid("gender")
 
  
 //get the elements 
+const birthday=birthdaybox.value;
 const birthmonth=birthmonthbox.value;
 const birthyear=birthyearbox.value;
 const birthgender=birthgenderbox.value;
 
 
-console.log(birthmonth,birthyear,birthgender)
+console.log(birthday,birthmonth,birthyear,birthgender)
+//check user gender
+const allgendersarrays=["male","female"]
+if (allgendersarrays.includes[birthgender]){
+    console.log("continue here")
+}
+else{
+    alert("please enter either male or female in the genderbox")
+
+}
 
 }
