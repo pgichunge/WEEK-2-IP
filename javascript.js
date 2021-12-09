1f4a4571a0f9db01b0eb1cba69713505dfedaad1
pamela mwendwa gichunge <pgichunge@safaricom.co.ke>
2021-12-09 23:59:57 +0300

    add changes

---
+++ javascript.js
@@ -0,0 +1,19 @@
+const handleclick=() =>{
+    // gather user input for
+
+const birthdaybox=document.getElementbyid("birthday")
+const birthmonthbox=document.getElementbyid("birthmonth")
+const birthyearbox=document.getElementbyid("birthyear")
+const birthgenderbox=document.getElementbyid("gender")
+
+ 
+//get the elements value
+const birthday=birthdaybox.value;
+const birthmonth=birthmonthbox.value;
+const birthyear=birthyearbox.value;
+const birthgender=birthgenderbox.value;
+
+
+console.log(birthday,birthmonth,birthyear,birthgender)
+
+}
