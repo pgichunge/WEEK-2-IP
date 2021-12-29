7f4b2ae68a0bf4fceedcc3d30a21686118553463
pamela mwendwa gichunge <pgichunge@safaricom.co.ke>
2021-12-29 21:41:30 +0300

    add changes

--- js/javascript.js
+++ js/javascript.js
@@ -1,77 +1,16 @@
+const handleclick= () => {
 
-//calculating akan name 
-// formular given 
+    //gather user input
+    const birthdaybox=document.getElementById['birthday']
+    const birthmonthbox=document.getElementById['birthmonth']
+    const birthyearbox=document.getElementById['birthyear']
+    const genderbox=document.getElementById['gender']
+    //get input element value
+    const birthday=birthdaybox.value;
+    const birthmonth=birthmonthbox.value;
+    const birthyear=birthyearbox.value;
+    const gender=genderbox.value;
+   
 
-function farLeft(CC) {
-    return ((CC / 4) - 2 * CC - 1);
-}
-
-function middle(YY) {
-    return (5 * YY / 4);
-}
-
-function farRight(MM) {
-    return (26 * (MM + 1) / 10);
-}
-
-var birthdayYear = Number(prompt("Which year were you born?"))
-var gender = prompt("What is your gender?")
-
-const farLeftResult = farLeft(10)
-const farRightResult = farRight(10)
-const middleResult = middle(birthdayYear)
-
-var calculationResult = (farLeftResult + middleResult + farRightResult + 10) % 7
-const wholeNumber = calculationResult.toPrecision(1);
-alert(wholeNumber);
-
-// calculationResult = 2
-const maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"]
-const femaleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"]
-
-let result;
-if (gender == "male") {
-    result = maleNames[wholeNumber]
-} else if (gender == "female") {
-    result = femaleNames[wholeNumber]
-} else {
-    alert('Enter a valid gender')
-}
-
-alert(`Your day name is ${result}`, )
-
-const handleclick = () => {
-    // gather user input 
-
-    constbirthdaybox = document.getelementbyid("birthday")
-    constbirthmonthbox = document.getelementbyid("birthmonth")
-    constbirthyearbox = document.getelementbyid("birthyear")
-    constbirthgenderbox = document.getelementbyid("gender")
-
-
-    //get the elements 
-    const birthday = birthdaybox.value;
-    const birthmonth = birthmonthbox.value;
-    const birthyear = birthyearbox.value;
-    const birthgender = birthgenderbox.value;
-
-
-    console.log(birthday, birthmonth, birthyear, birthgender)
-    //check user gender
-    const allgendersarrays = ["male", "female"]
-    if (allgendersarrays.includes(birthgender)) {
-        // CC - is the century digits
-        //YY - is the Year digit
-        const centuryvar = string(birthyear).substring[0, 2]
-        const yearvar = string(birthyear).substring[2]
-        console.log(century, year)
-        git
-
-    
-    }
-    else {
-        alert("please enter either male or female in the genderbox")
-
-    }
-
+    console.log(birthday, birthmonth, birthyear, gender)
 }
